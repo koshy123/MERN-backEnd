44e9abceb3cc2f714876e33d2e9515465240e8a4
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-12-02 14:16:58 -0500

    add index.js

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-const express = require('express')
-const app = express()
+// Require necessary NPM packages
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// Instantiate express application object
+const app = express();
+
+// The `.use` method sets up middleware in Express
+
+// Set up cors middleware and make sure that it
+// comes before our routes are used.
+app.use(cors());
+
+// Add `express.json` middleware which will
+// parse JSON requests into JS objects before
+// they reach the route files.
+app.use(express.json());
+
+// The urlencoded middleware parses requests which use
+// a specific content type (such as when using Axios)
+app.use(express.urlencoded({ extended: true }));
+
+// Define a port for API to run on, if the environment
+// variable called `PORT` is not found use port 4000
+app.set('port', process.env.PORT || 4000);
+// Run server on designated port
+app.listen(app.get('port'), () => {
+  console.log('listening on port ' + app.get('port'));
+});
