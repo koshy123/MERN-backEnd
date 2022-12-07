27c24a59f5795d8b99976cdb44bf678b12a11fa6
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-12-06 19:17:25 -0500

    The back end is now fully deployed on FLY

--- db/connection.js
+++ db/connection.js
@@ -13,20 +13,14 @@ const mongoURI = process.env.DATABASE_URL
 
 // Use Mongoose's connect method to connect to MongoDB by passing it the db URI.
 // Pass a second argument which is an object with the options for the connection.
-mongoose
-  .connect(mongoURI, {
-    // useNewUrlParser: true,
-    // useCreateIndex: true,
-    // useUnifiedTopology: true,
-    // useFindAndModify: false,
-  })
-  // If the connection is successful, give a message in the Terminal with the db name
+
+  mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((instance) =>
     console.log(`Connected to db: ${instance.connections[0].name}`)
   )
-  // If the connection fails, give a message and pass along the error so we see it in
-  // the Terminal.
   .catch((error) => console.log('Connection failed!', error));
+
 
 // Export the connection so we can use it elsewhere in our app.
 module.exports = mongoose;
